df072060060426a45519906d4db42f5a8576cd8e
Iago <tu_correo@ejemplo.com>
2026-04-21 12:05:22 +0200

    Fix frozen_lake and main

--- env/frozen_lake_env.py
+++ env/frozen_lake_env.py
@@ -1,4 +1,6 @@
 import gymnasium as gym
+from resources.config import MAP_NAME, IS_SLIPPERY, RENDER_MODE
+
 def create_env():
     """Creates a Frozen Lake environment.
 
@@ -7,10 +9,9 @@ def create_env():
     """
     env = gym.make(
         'FrozenLake-v1',
-        desc=None , 
-        map_name="4x4",
-        is_slippery=False,
-        render_mode="human"
+        desc=None,
+        map_name=MAP_NAME,
+        is_slippery=IS_SLIPPERY,
+        render_mode=RENDER_MODE
     )
-    return env 
-
+    return env
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import time 
+import time
 from env.frozen_lake_env import create_env
 from agents.random_agent import RandomAgent
 from resources.config import PAUSE_BETWEEN_STEPS
 
-def execute_mission_u_boat():
-    """ Executes the mission for the U-Boat agent in the Frozen Lake environment.
-        The agent will take random actions until it either reaches the goal or falls into a hole. 
-        The environment is rendered at each step, and the outcome of the mission is printed at the end.
+def run_frozen_lake_episode():
+    """Executes an episode in the Frozen Lake environment.
+
+    The agent takes random actions until it either reaches the goal or falls into a hole.
+    The environment is rendered at each step, and the outcome is printed at the end.
     """
     env = create_env()
     agent = RandomAgent(env.action_space)
@@ -24,11 +25,10 @@ def execute_mission_u_boat():
         time.sleep(PAUSE_BETWEEN_STEPS)
 
     if reward == 1:
-        print(f"Mission accomplished in {steps} steps!")
+        print(f"Goal reached in {steps} steps!")
     else:
-        print(f"Mission failed after {steps} steps.")
+        print(f"Fell into a hole after {steps} steps.")
     env.close()
 
 if __name__ == "__main__":
-    execute_mission_u_boat()
-
+    run_frozen_lake_episode()
